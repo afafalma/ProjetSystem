3094a86b41f728a45ae9051900fd4165289b8c50
sanaa <sanaa@sanaa-PC>
2012-06-06 02:20:43 -0430

    fonction permettant de faire le chargement des données d'un assignement dans un vector

--- SimulationProcessus/src/sauvegarde/ChargementAssignment.java
+++ SimulationProcessus/src/sauvegarde/ChargementAssignment.java
@@ -1,5 +1,71 @@
 package sauvegarde;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamException;
+import java.util.Vector;
+
+import model.Assignement;
+
+
+/*
+ * cette classe permet de charger les informations sur les assignements sauvegardés
+ */
+
 public class ChargementAssignment {
 
+	
+	public static void load(Vector<Assignement> vector)
+	{
+
+		try {
+			
+			
+			File f = new File("simulationProcessus");
+			
+			
+			try {
+				if (!f.exists())
+				
+					f.mkdirs();
+				
+
+			} catch (Exception e) {
+			}
+			FileInputStream fis = new FileInputStream("simulationProcessus/sauvegardeAssignement.dat");
+		
+			
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			
+		
+			try {
+				
+				while (true) {
+					try {
+						
+						Assignement ass = (Assignement) ois.readObject();
+						vector.add(ass);
+						
+
+					} catch (ObjectStreamException ose) {
+						return;
+					} catch (ClassNotFoundException cnfe) {
+						return;
+					}
+				}
+			} catch (EOFException eof) {
+			}
+			ois.close();
+			fis.close();
+
+		} catch (Exception ex) {
+			return;
+		}
+		
+		
+
+	}
 }
